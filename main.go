1653897e2bb2c63584953392eac5301798b70ad6
David Ives <davidives@Davids-MacBook-Pro.local>
2019-01-17 11:05:29 +0000

    New test routes added

--- main.go
+++ main.go
@@ -7,6 +7,15 @@ import (
 // Define a home handle function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exacrly matches "/" If it doesn't,
+	// use the http.NotFound() function to send a 404 to the client
+	// Importantly, we then return from the handler. If we don't return, the handler
+	// would keep executing and also write the "Hello from Snippetbox" message.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	
 	w.Write([]byte("Hello from Snippetbox"))
 }
 

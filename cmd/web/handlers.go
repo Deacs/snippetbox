ed9cffaf01e7dab04d6e3c0c62e85bbb77b492d3
David Ives <davidives@Davids-MacBook-Pro.local>
2019-01-25 12:05:55 +0000

    Template conditionals and loops used to display latest Snippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -23,40 +23,34 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 	}
 
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
+	data := &templateData{Snippets: s}
+
+	// Initialize a slice containing the paths to the two files.
+	// Note tha the home.page.tmpl file must be the *first* file in the slice
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
 	}
 
-	/*
+	// Use the template.ParseFiles() function to read the template file into a template set.
+	// If there's an error, we log the detailed error message and use the
+	// http.Error() function to send a generic 500 Internal Server Error response to the user
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		// Now using the serverError() helper
+		app.serverError(w, err)
+		return
+	}
 
-		// Initialize a slice containing the paths to the two files.
-		// Note tha the home.page.tmpl file must be the *first* file in the slice
-		files := []string{
-			"./ui/html/home.page.tmpl",
-			"./ui/html/base.layout.tmpl",
-			"./ui/html/footer.partial.tmpl",
-		}
-
-		// Use the template.ParseFiles() function to read the template file into a template set.
-		// If there's an error, we log the detailed error message and use the
-		// http.Error() function to send a generic 500 Internal Server Error response to the user
-		ts, err := template.ParseFiles(files...)
-		if err != nil {
-			// Now using the serverError() helper
-			app.serverError(w, err)
-			return
-		}
-
-		// We then we use the Execute() method on the template set to write the template
-		// content as the response body. The last parameter to Execute() represents any
-		// dynamic data that we want to pass in, which for now, we'll leave as nil
-		err = ts.Execute(w, nil)
-		if err != nil {
-			// Now using the serverError() helper
-			app.serverError(w, err)
-		}
-
-	*/
+	// We then we use the Execute() method on the template set to write the template
+	// content as the response body. The last parameter to Execute() represents the
+	// templateData struct initialized above
+	err = ts.Execute(w, data)
+	if err != nil {
+		// Now using the serverError() helper
+		app.serverError(w, err)
+	}
 }
 
 // Change the signature of the handler so it is defined as a method against *application

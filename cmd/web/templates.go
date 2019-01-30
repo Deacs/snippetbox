1a08a40f1c27db1c629b6fa8a28e78d2b2a7f922
David Ives <davidives@Davids-MacBook-Pro.local>
2019-01-30 09:04:45 +0000

    Custom template functions set up and registered

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"chilliweb.com/snippetbox/pkg/models"
 )
@@ -10,10 +11,22 @@ import (
 // Define a templateData type to act as the holding structure for
 // any dynamic data that we want to pass to our HTML templates.
 type templateData struct {
-	// Add a CurrentYear field to the struct
-	CurrentYear int
+	CurrentYear int // Add a CurrentYear field to the struct
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+}
+
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time object.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialze a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custom template functions and the functions themselves
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -34,8 +47,11 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 		// and assign to a var
 		name := filepath.Base(page)
 
-		// Parse the page template file in to a template set.
-		ts, err := template.ParseFiles(page)
+		// The template.FuncMap must be registered with the template set before we
+		// call the ParseFiles() method. This means we have to use template.New() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and parse the file as normal
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}

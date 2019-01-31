306ba46be2aea0fddae654b25ad27c011c2ed8af
David Ives <davidives@Davids-MacBook-Pro.local>
2019-01-31 14:42:12 +0000

    Composable middelware chains added using the Alice package

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,10 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // Updated signature for the routes() method as it now returns
 // a http.Handler instead of the original *http.ServeMux
 func (app *application) routes() http.Handler {
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -13,6 +21,6 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// Wrap the exisiting chain in the recoverPanic() middleware.
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	// Return the standard middleware chain followed by the servemux.
+	return standardMiddleware.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ require (
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/google/pprof v0.0.0-20190109223431-e84dfd68c163 // indirect
 	github.com/ianlancetaylor/demangle v0.0.0-20181102032728-5e5cf60278f6 // indirect
+	github.com/justinas/alice v0.0.0-20171023064455-03f45bd4b7da
 	golang.org/x/arch v0.0.0-20181203225421-5a4828bb7045 // indirect
 	golang.org/x/crypto v0.0.0-20190103213133-ff983b9c42bc // indirect
 	golang.org/x/sys v0.0.0-20190116161447-11f53e031339 // indirect
